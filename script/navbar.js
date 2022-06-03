c9820f43f512f16f2bbbff347b026114e34938f1
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-06-03 23:59:31 +0700

    Update

--- script/navbar.js
+++ script/navbar.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 
     $(".categ").hover(function(){
-        $(".panah-bawah").removeClass('kelasPanahBawah');
-        $(".panah-bawah").removeClass('panah-bawah-warna');
-        $(".panah-bawah").addClass('kelasPanahBawahHover');
+        if(window.location.name !== "category"){
+            $(".panah-bawah").removeClass('kelasPanahBawah');
+            $(".panah-bawah").removeClass('panah-bawah-warna');
+            $(".panah-bawah").addClass('kelasPanahBawahHover');
+        }
     }, function(){
-        $(".panah-bawah").removeClass('kelasPanahBawahHover');
-        $(".panah-bawah").addClass('kelasPanahBawah');
+        if(window.location.name !== "category"){
+            $(".panah-bawah").removeClass('kelasPanahBawahHover');
+            $(".panah-bawah").addClass('kelasPanahBawah');
+        }
     });
     
     $(".categ").click(function(){
@@ -17,24 +21,34 @@ $(document).ready(function(){
         }
     });
     
+    $('.left').click(function(){
+        window.location.name = "home"
+        $('#root').load('home.html');
+    });
+
     $('.navbar-home').click(function(){
+        window.location.name = "home"
         $('#root').load('home.html');
     });
 
     $('.navbar-top-author').click(function(){
+        window.location.name = "topAuthor"
         $('#root').load('topAuthor.html');
     });
 
     $('.navbar-top-mebtoon').click(function(){
+        window.location.name = "topMebtoon"
         $('#root').load('topMebtoon.html');
     });
 
     $('.navbar-subscribe').click(function(){
+        window.location.name = "subscribe"
         $('#root').load('subscribe.html');
     });
 
     $('.navbar-category-show-all').click(function(){
-        $('#root').load('categoryShowAll.html');
+        window.location.name = "category"
+        $('#root').load('category.html');
     });
 
     $('#burgerMenu').click(function (){
@@ -46,5 +60,8 @@ $(document).ready(function(){
         }
     })
 
-
+    loadTopMebtoon =(index)=>{
+        $('#root').load('topMebtoon.html');
+        currentCategory = index;
+    }
 });
