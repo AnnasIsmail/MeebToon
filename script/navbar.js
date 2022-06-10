ade86b859cf0733c3cdbfa6b95bb80046d81ecf7
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-06-10 21:01:38 +0700

    Update and Fix Error

--- script/navbar.js
+++ script/navbar.js
@@ -24,31 +24,37 @@ $(document).ready(function(){
     $('.left').click(function(){
         window.location.name = "home"
         $('#root').load('home.html');
+        window.scrollTo(0, 0);
     });
 
     $('.navbar-home').click(function(){
         window.location.name = "home"
         $('#root').load('home.html');
+        window.scrollTo(0, 0);
     });
 
     $('.navbar-top-author').click(function(){
         window.location.name = "topAuthor"
         $('#root').load('topAuthor.html');
+        window.scrollTo(0, 0);
     });
 
     $('.navbar-top-mebtoon').click(function(){
         window.location.name = "topMebtoon"
         $('#root').load('topMebtoon.html');
+        window.scrollTo(0, 0);
     });
 
     $('.navbar-subscribe').click(function(){
         window.location.name = "subscribe"
         $('#root').load('subscribe.html');
+        window.scrollTo(0, 0);
     });
 
     $('.navbar-category-show-all').click(function(){
         window.location.name = "category"
         $('#root').load('category.html');
+        window.scrollTo(0, 0);
     });
 
     $('#burgerMenu').click(function (){
@@ -64,11 +70,13 @@ $(document).ready(function(){
         window.location.name = "topMebtoon"
         $('#root').load('topMebtoon.html');
         currentCategory = index;
+        window.scrollTo(0, 0);
     }
 
     loadDetailMebtoon =(id)=>{
         window.location.name = "detail-mebToon"
         window.location.id = id
         $('#root').load('detailMebtoon.html');
+        window.scrollTo(0, 0);
     }
 });
